5fe02e76db71cacd0d0ab3a88a423d3e57739e2f
romfantast <romfantast@gmail.com>
2023-04-29 14:29:47 +0300

    add style for arrow dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -6,14 +6,22 @@ const paragraphTitles = [aboutUsTitle, serviceTitle, usefullTitle];
 
 for (let i = 0; i < paragraphTitles.length; i += 1) {
   paragraphTitles[i].addEventListener('click', () => {
-    console.log(paragraphTitles[i].attributes[0].nodeName.split('-'));
     const listName = paragraphTitles[i].attributes[0].nodeName
       .toString()
       .split('-')[2];
     const element = document.querySelector(
       `.footer-navigation-block div ul[data-${listName}-list]`
     );
-    console.log(element);
-    element.classList.toggle('dropdown');
+    const arrow = document.querySelector(
+      `.footer-navigation-block div p[data-about-${listName}]`
+    );
+
+    if (element.classList.contains('dropdown')) {
+      element.classList.remove('dropdown');
+      arrow.style.setProperty('--rotate-arrow', 'rotate(0deg)');
+    } else {
+      element.classList.add('dropdown');
+      arrow.style.setProperty('--rotate-arrow', 'rotate(180deg)');
+    }
   });
 }
